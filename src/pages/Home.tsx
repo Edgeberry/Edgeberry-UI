dbbced7c24db0994f2aec5b9693c42ecae8968c3
Tom Santens <tom.santens@gmail.com>
2024-03-28 13:45:21 +0100

    Home: tabs

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,21 @@
+import { Alert, Container, Tab, Tabs } from "react-bootstrap";
 
 const Home = ()=>{
     return(
-        <>
+        <Container>
             <h1>Edge Gateway UI</h1>
-        </>
+            <Tabs>
+                <Tab eventKey="connectivityTab" title={'Connectivity'}>
+                    <Alert>Here comes everything related to connectivity with the cloud</Alert>
+                </Tab>
+                <Tab eventKey="applicationTab" title={'Application'}>
+                    <Alert>Here comes everything related to the device application</Alert>
+                </Tab>
+                <Tab eventKey="systemTab" title={'System'}>
+                    <Alert>Here comes everything related to the device's system</Alert>
+                </Tab>
+            </Tabs>
+        </Container>
     );
 }
 
