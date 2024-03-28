ebb09826f02616f1aa5657a74bd4e55a28079795
Tom Santens <tom.santens@gmail.com>
2024-03-28 13:35:48 +0100

    Bootstrap css and scripts from CDN

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 
 const NavigationBar = ()=>{
     return(
-        <Navbar sticky="top">
+        <Navbar sticky="top" className="navbar-dark bg-dark">
             <Container>
-                <Link className="navbar-brand" to='/'>
-                    <div className="navbar-text">Edge Gateway</div>
+                <Link className="navbar-brand navbar-dark bg-dark" to='/'>
+                    <div className="navbar-text navbar-dark">Edge Gateway</div>
                 </Link>
             </Container>
         </Navbar>
