def66093fa555daa0919d766d072fccf6302a315
Tom Santens <tom.santens@gmail.com>
2024-03-29 13:49:41 +0100

    REST API: system features implementation

---
+++ src/api/system.tsx
@@ -0,0 +1,52 @@
+/*
+ *  System REST API
+ */
+
+/* Network */
+
+/* Get Azure Device Provisioning Service for IoT Hub parameters */
+export async function api_system_getNetworkSettings(){
+    const response = await fetch( window.location.origin+'/api/system/network/settings',{
+        method: 'GET',
+        headers: {'Content-Type': 'application/json'},
+        credentials: 'include'
+    });
+    try{
+        const content = await response.json()
+        return content;
+    } catch(err){
+        return {message:err};
+    }
+}
+
+/* System */
+
+/* Get system application info */
+export async function api_system_getApplicationInfo(){
+    const response = await fetch( window.location.origin+'/api/system/application/info',{
+        method: 'GET',
+        headers: {'Content-Type': 'application/json'},
+        credentials: 'include'
+    });
+    try{
+        const content = await response.json()
+        return content;
+    } catch(err){
+        return {message:err};
+    }
+}
+
+/* Reboot the system */
+export async function api_system_reboot(){
+    const response = await fetch( window.location.origin+'/api/system/reboot',{
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        credentials: 'include'
+    });
+    try{
+        const content = await response.json()
+        return content;
+    } catch(err){
+        return {message:err};
+    }
+}
