094e34e4332506122abfd13a4eb45144f1bf7381
Tom Santens <tom.santens@gmail.com>
2024-03-29 08:37:38 +0100

    Application: UI components

--- src/pages/Application.tsx
+++ src/pages/Application.tsx
@@ -1,10 +1,49 @@
-import { Alert, Container } from "react-bootstrap";
+import { useState } from "react";
+import { Alert, Button, Col, Container, Form, Row } from "react-bootstrap";
 
 const Application = ()=>{
+    const[ disabled, setDisabled ] = useState<boolean>(false);
+    const[ repository, setRepository ] = useState<string>('');
+    const[ accessToken, setAccessToken ] = useState<string>('');
+    const[ appName, setAppName ] = useState<string>('');
+    const[ appVersion, setAppVersion ] = useState<string>('');
+
     return(
         <Container>
-            <h2>Application</h2>
-            <Alert>Here comes everything related to the device's application</Alert>
+            <div className="float-right">
+                <Button variant={'danger'}>Restart</Button>
+            </div>
+            <h1>Application</h1>
+            <Form.Group as={Row} className="mb-2">
+                    <Form.Label column sm={2}>Repository</Form.Label>
+                    <Col sm={6}>
+                        <Form.Control type={'text'} placeholder={'Repository'} value={repository} onChange={(e)=>{setRepository(e.target.value)}} required disabled={disabled}/>
+                    </Col>
+                </Form.Group>
+                <Form.Group as={Row} className="mb-2">
+                    <Form.Label column sm={2}>Access Token</Form.Label>
+                    <Col sm={6}>
+                        <Form.Control type={'text'} placeholder={'Access Token'} value={accessToken} onChange={(e)=>{setAccessToken(e.target.value)}} required disabled={disabled}/>
+                    </Col>
+                </Form.Group>
+                <Form.Group as={Row} className="mb-2">
+                    <Form.Label column sm={2}>Application name</Form.Label>
+                    <Col sm={6}>
+                        <Form.Control type={'text'} placeholder={'Application name'} value={appName} disabled/>
+                    </Col>
+                </Form.Group>
+                <Form.Group as={Row} className="mb-2">
+                    <Form.Label column sm={2}>Version</Form.Label>
+                    <Col sm={6}>
+                        <Form.Control type={'text'} placeholder={'Version'} value={appVersion} disabled/>
+                    </Col>
+                </Form.Group>
+                <Form.Group as={Row} className="mb-2">
+                    <Form.Label column sm={2}></Form.Label>
+                    <Col sm={6}>
+                        <Button variant={'danger'}>Update</Button>
+                    </Col>
+                </Form.Group>
         </Container>
     );
 }
