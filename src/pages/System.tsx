426b2cae5c56fb8b1ebc3705f89a97d1cbae18e0
Tom Santens <tom.santens@gmail.com>
2024-03-29 13:59:38 +0100

    System features implementation

--- src/pages/System.tsx
+++ src/pages/System.tsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button, Col, Container, Form, Row } from "react-bootstrap";
+import { api_system_getApplicationInfo, api_system_getNetworkSettings, api_system_reboot } from "../api/system";
 
 const System = ()=>{
     // Network
@@ -8,10 +9,51 @@ const System = ()=>{
     // System application
     const[ appVersion, setAppVersion ] = useState<string>('');
 
+    useEffect(()=>{
+        getNetworkSettings();
+        getSystemApplicationInfo();
+    },[]);
+
+    // Get the network settings
+    async function getNetworkSettings(){
+        const result = await api_system_getNetworkSettings();
+        if( result.message ){
+            //Todo: handle error
+            return window.alert(result.message);
+        }
+        if( typeof(result.ssid) === 'string' ) setSsid(result.ssid);
+        if( typeof(result.ipAddress ) === 'string' ) setIpAddress(result.ipAddress);
+        return;
+    }
+
+    // Get the system application info
+    async function getSystemApplicationInfo(){
+        const result = await api_system_getApplicationInfo();
+        if( result.message ){
+            //Todo: handle error
+            return window.alert(result.message);
+        }
+        if( typeof(result.version) === 'string' ) setAppVersion(result.version);
+        return;
+    }
+
+    // Request system restart
+    async function requestSystemRestart(){
+        // Check if user is sure about this action
+        if( !window.confirm("Restart system?") ) return;
+
+        const result = await api_system_reboot();
+        if( result.message ){
+            //Todo: handle error
+            return window.alert(result.message);
+        }
+        return;
+    }
+
     return(
         <Container>
             <div className="float-right">
-                <Button variant={'danger'}>Restart</Button> &nbsp;
+                <Button variant={'danger'} onClick={()=>{requestSystemRestart()}}>Restart</Button> &nbsp;
                 <Button variant={'primary'}>Identify</Button>
             </div>
 
